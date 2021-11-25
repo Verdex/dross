d31deb0ebc8888c61a2576a25c47e7e2f3394f90
David L. Inman <davidlevere@gmail.com>
2021-11-24 20:11:40 -0500

    initial files

---
+++ data_match.py
@@ -0,0 +1,17 @@
+
+class Data:
+    def __init__(self):
+        self.name = ''
+        self.args = {}
+
+class Matcher:
+    pass
+
+    def match(self, data):
+        # type(data) is str
+        # type(data) is Data
+        pass
+
+
+def parse_matcher(str):
+    pass
