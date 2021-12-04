ebce6f71b22980e1b0bc564c81c2b05b9ec55ba9
David L. Inman <davidlevere@gmail.com>
2021-12-03 23:02:49 -0500

    parse tests

--- src/data_match/parse_test.py
+++ src/data_match/parse_test.py
@@ -23,6 +23,37 @@ class ParseTest(unittest.TestCase):
         self.assertTrue(type(output) is MatchStringWithValue)
         self.assertEqual(output.value, "string")
 
+    def test_parse_matcher_should_generate_match_data_with_name(self):
+        output = parse_matcher("string()")
+        self.assertTrue(type(output) is MatchDataWithName)
+        self.assertEqual(output.target_name, "string")
+
+    def test_parse_matcher_should_generate_capture_data(self):
+        output = parse_matcher("String()")
+        self.assertTrue(type(output) is CaptureData)
+        self.assertEqual(output.capture_name, "String")
+
+    def test_parse_matcher_should_generate_capture_data_with_sub_matchers(self):
+        output = parse_matcher("String(a, ., X(), a(), *)")
+        self.assertTrue(type(output) is CaptureData)
+        self.assertEqual(output.capture_name, "String")
+        self.assertEqual(len(output.sub_matchers), 5)
+        self.assertTrue(type(output.sub_matchers[0]) is MatchStringWithValue)
+        self.assertTrue(type(output.sub_matchers[1]) is MatchAnyString)
+        self.assertTrue(type(output.sub_matchers[2]) is CaptureData)
+        self.assertTrue(type(output.sub_matchers[3]) is MatchDataWithName)
+        self.assertTrue(type(output.sub_matchers[4]) is MatchUntilEnd)
+
+    def test_parse_matcher_should_generate_match_data_with_name_with_sub_matchers(self):
+        output = parse_matcher("string(a, ., X(), a(), *)")
+        self.assertTrue(type(output) is MatchDataWithName)
+        self.assertEqual(output.target_name, "string")
+        self.assertEqual(len(output.sub_matchers), 5)
+        self.assertTrue(type(output.sub_matchers[0]) is MatchStringWithValue)
+        self.assertTrue(type(output.sub_matchers[1]) is MatchAnyString)
+        self.assertTrue(type(output.sub_matchers[2]) is CaptureData)
+        self.assertTrue(type(output.sub_matchers[3]) is MatchDataWithName)
+        self.assertTrue(type(output.sub_matchers[4]) is MatchUntilEnd)
 
 if __name__ == '__main__':
     unittest.main()
